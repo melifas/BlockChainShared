f55c64630698da1d28b78e3d2cd37d227566f3ea
melifas <meletisfasoulis@yahoo.gr>
2019-11-27 18:51:49 +0200

    first commit

---
+++ src/pojo/Product.java
@@ -0,0 +1,133 @@
+package pojo;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Product {
+
+    private static Logger logger = Logger.getLogger(Product.class.getName());
+
+
+    private String hash;
+    private String previousHash;
+    private int TimeStamp;
+    private int nonce;
+
+    private int id;
+
+    private int CodeOfProduct;
+    private String TitleOfProduct;
+    private int Price;
+    private String DescriptionOfProduct;
+
+    public Product() {
+    }
+
+
+//--------- ctor με την data και τα απαραίτητα πεδία δημιουργίας του blockchain--------
+
+    public Product(int id,int codeOfProduct,String titleOfProduct,int price,String descriptionOfProduct, int timeStamp,String previousHash) {
+        this.id = id;
+        CodeOfProduct = codeOfProduct;
+        this.previousHash = previousHash;
+        TimeStamp = timeStamp;
+        TitleOfProduct = titleOfProduct;
+        Price = price;
+        DescriptionOfProduct = descriptionOfProduct;
+        hash = calculateBlockHash();
+    }
+    //--------- ctor με την data και τα απαραίτητα πεδία δημιουργίας του blockchain ΑΛΛΑ  ΧΩΡΙΣ ΤΟ ID--------
+    public Product(int codeOfProduct,String titleOfProduct,int price,String descriptionOfProduct, int timeStamp,String previousHash) {
+        hash = calculateBlockHash();
+        this.previousHash = previousHash;
+        TimeStamp = timeStamp;
+        this.nonce = nonce;
+        CodeOfProduct = codeOfProduct;
+        TitleOfProduct = titleOfProduct;
+        Price = price;
+        DescriptionOfProduct = descriptionOfProduct;
+    }
+
+    //------- Πλήρες ctor---------------
+
+    public Product(int id,int codeOfProduct,String titleOfProduct,int price,String descriptionOfProduct, int timeStamp,String previousHash,String hash) {
+        this.hash = hash;
+        this.previousHash = previousHash;
+        TimeStamp = timeStamp;
+        this.id = id;
+        CodeOfProduct = codeOfProduct;
+        TitleOfProduct = titleOfProduct;
+        Price = price;
+        DescriptionOfProduct = descriptionOfProduct;
+    }
+
+    public String mineBlock(int prefix) {
+        String prefixString = new String(new char[prefix]).replace('\0', '0');
+        while (!hash.substring(0, prefix).equals(prefixString)) {
+            nonce++;
+            hash = calculateBlockHash();
+        }
+        return hash;
+    }
+
+    public String calculateBlockHash() {
+        String dataToHash = previousHash + Long.toString(TimeStamp) + Integer.toString(nonce) + Integer.toString(id) + Integer.toString(CodeOfProduct) + TitleOfProduct+ Integer.toString(Price) + DescriptionOfProduct;
+        MessageDigest digest = null;
+        byte[] bytes = null;
+        try {
+            digest = MessageDigest.getInstance("SHA-256");
+            bytes = digest.digest(dataToHash.getBytes("UTF-8"));
+        } catch (NoSuchAlgorithmException | UnsupportedEncodingException ex) {
+            logger.log(Level.SEVERE, ex.getMessage());
+        }
+        StringBuffer buffer = new StringBuffer();
+        for (byte b : bytes) {
+            buffer.append(String.format("%02x", b));
+        }
+        return buffer.toString();
+    }
+
+    public static Logger getLogger() {
+        return logger;
+    }
+
+    public String getHash() {
+        return hash;
+    }
+
+    public String getPreviousHash() {
+        return previousHash;
+    }
+
+    public int getTimeStamp() {
+        return TimeStamp;
+    }
+
+    public int getNonce() {
+        return nonce;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getCodeOfProduct() {
+        return CodeOfProduct;
+    }
+
+    public String getTitleOfProduct() {
+        return TitleOfProduct;
+    }
+
+    public int getPrice() {
+        return Price;
+    }
+
+    public String getDescriptionOfProduct() {
+        return DescriptionOfProduct;
+    }
+}
